cc6871989dc7da8ed450b77a4284c26c27aed5e1
tuandeptrai <atuannguyen1706@gmail.com>
2025-08-28 17:44:00 +0700

    fix merge conflict

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-<<<<<<< HEAD
   await app.listen(80, '0.0.0.0');
-=======
-  // await app.useGlobalPipes(
-  //   new ValidationPipe({
-  //     transform: true,
-  //     whitelist: true,
-  //     forbidNonWhitelisted: true,
-  //   }),
-  // );
-  await app.listen(process.env.PORT ?? 3000);
->>>>>>> f4cf998 (test)
 }
 bootstrap();
