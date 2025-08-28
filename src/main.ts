eebaf5a084e8462e23389343b2e24f0e0210a0a2
tuandeptrai <atuannguyen1706@gmail.com>
2025-08-28 15:44:22 +0700

    test

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(80, '0.0.0.0'); // <-- thêm '0.0.0.0'
+<<<<<<< HEAD
+  await app.listen(80, '0.0.0.0');
+=======
+  // await app.useGlobalPipes(
+  //   new ValidationPipe({
+  //     transform: true,
+  //     whitelist: true,
+  //     forbidNonWhitelisted: true,
+  //   }),
+  // );
+  await app.listen(process.env.PORT ?? 3000);
+>>>>>>> f4cf998 (test)
 }
 bootstrap();
