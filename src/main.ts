d46fd9c415e37db0161b390a2735e5a94a60fab3
tuanna-day1 <tuannguyen@dayoneteams.com>
2025-09-03 17:57:00 +0700

    .

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3001;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   console.log(`App running on port ${port}`);
 }
 bootstrap();
